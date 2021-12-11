67eeb7d26ccec8412da0400b000a27cfade750d8
MOHAMMAD TANVIR HASAN <77626093+tanvir1546@users.noreply.github.com>
2021-12-11 16:24:31 +0900

    Update install-prerequisites.sh

--- jetson_nano/install-prerequisites.sh
+++ jetson_nano/install-prerequisites.sh
@@ -4,15 +4,12 @@ pip3 install wheel
 sudo apt-get install libhdf5-dev libhdf5-serial-dev
 sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt-get install libatlas-base-dev
-python3 -m pip install --upgrade pip
 pip3 install numpy
 python3 -m pip install --upgrade Pillow
 #pip3 install matplotlib
 pip3 install pandas
-
 # Installs OpenCV pip package
 pip install opencv-python
-
 # Install the tflite_runtime
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
